b5bec223a38a94a81cbe625631b83096fa1b071f
Joakim Bick <joakim.bick@gmail.com>
2021-12-12 20:55:18 +0100

    Initial files

---
+++ 10a.js
@@ -0,0 +1,32 @@
+function solve(input) {
+	var lines = input.split("\n")
+		.map(l => l.split(""));
+	var starters = { '{': '}', '[': ']', '<': '>', '(': ')' };
+	var enders = { '}': '{', ']': '[', '>': '<', ')': '(' };
+
+	var scores = { ')': 3, ']': 57, '}': 1197, '>': 25137 };
+
+	var errorLines = lines.map(line => {
+		let context = [];
+		for(let i = 0; i < line.length; i++) {
+			
+			var currChar = line[i];
+			if(context.length == 0 || starters.hasOwnProperty(currChar)) {
+				context.push(currChar);
+				continue;
+			}
+
+			if(enders[currChar] === context[context.length - 1]) {
+				context.pop();
+				continue;
+			}
+
+			console.log(context.join(""), currChar, scores[currChar]);
+			return scores[currChar];
+		}
+
+		return 0;
+	}).reduce((a, b) => a + b, 0);
+
+	return errorLines;
+}
---
+++ 10b.js
@@ -0,0 +1,35 @@
+function solve(input) {
+	var lines = input.split("\n")
+		.map(l => l.split(""));
+	var starters = { '{': '}', '[': ']', '<': '>', '(': ')' };
+	var enders = { '}': '{', ']': '[', '>': '<', ')': '(' };
+
+	var scores = { ')': 3, ']': 57, '}': 1197, '>': 25137 };
+	var autoCompleteScores = { ')': 1, ']': 2, '}': 3, '>': 4 };
+
+	var errorLines = lines.map(line => {
+		let context = [];
+		for(let i = 0; i < line.length; i++) {
+			
+			var currChar = line[i];
+			if(context.length == 0 || starters.hasOwnProperty(currChar)) {
+				context.push(currChar);
+				continue;
+			}
+
+			if(enders[currChar] === context[context.length - 1]) {
+				context.pop();
+				continue;
+			}
+
+			return 0;
+		}
+
+		return context.reverse().reduce((p, o) => {
+			return p * 5 + autoCompleteScores[starters[o]];
+		}, 0);
+
+	}).filter(x => x > 0).sort((a,b) => a - b);
+	console.log(errorLines);
+	return errorLines[Math.floor(errorLines.length/2)];
+}
